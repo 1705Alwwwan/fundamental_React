d2ae975b290c56d5022db1d8cd84cfbc826902be
Alwan Farhan Aziz <155723244+1705Alwwwan@users.noreply.github.com>
2025-05-17 07:56:09 +0700

    Fundamental_REACTJS
    sutdy and learn about react concept

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main style={{ padding: '1rem' }}>
+        <h2>Welcome to My Website!</h2>
+        <p>This is a simple web page made with React.</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
